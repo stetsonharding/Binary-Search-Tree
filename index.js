93282c8d53b2dc2bf34d1bf39339d30d181108a7
stetsonharding <stetson2001@msn.com>
2022-03-21 08:17:26 -0700

    Searching and visit implementation.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+//Binary Trees is a data structure.
+//trees have a root node which each node has at most 2 children that are refered to as left or right child.
+//If the value coming in is greater than then the root, that value is stored as the right child, if it is less, that value is stored as the left child.
+
+//   5
+//   ^
+//  3  4
+
 var tree;
 function setup() {
   tree = new Tree();
-  tree.addValue(5);
-  tree.addValue(3);
-  tree.addValue(7);
+
+  for (let i = 0; i < 10; i++) {
+    let ran = Math.floor(Math.random() * (0, 10));
+    tree.addValue(ran);
+  }
 
   console.log(tree);
+
+  tree.traverse();
+
+  tree.search(3);
 }
 
 //Tree
@@ -13,12 +27,48 @@ function Tree() {
   this.root = null;
 }
 
+Tree.prototype.traverse = function () {
+  this.root.visit();
+};
+
+Tree.prototype.search = function (val) {
+  this.root.search(val);
+};
+
 Tree.prototype.addValue = function (n) {
   let node = new Node(n);
   if (this.root === null) {
     this.root = node;
   } else {
     this.root.addNode(node);
+  }
+};
+
+// =====
+
+//Node
+
+Node.prototype.search = function (val) {
+  if (this.value === val) {
+    console.log("found: " + val);
+  } else if (val < this.value && this.left !== null) {
+    this.left.search(val);
+  } else if (val > this.value && this.right !== null) {
+    this.right.search(val);
+  } else {
+    console.log(`value ${val} not found.`);
+  }
+};
+
+Node.prototype.visit = function () {
+  if (this.left != null) {
+    this.left.visit();
+  }
+
+  console.log(this.value);
+
+  if (this.right != null) {
+    this.right.visit();
   }
 };
 
@@ -38,11 +88,12 @@ Node.prototype.addNode = function (n) {
   }
 };
 
-//Node
 function Node(val) {
   this.value = val;
   this.left = null;
   this.right = null;
 }
+
+// =====
 
 setup();
